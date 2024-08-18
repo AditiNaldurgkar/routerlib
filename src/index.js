2f6c15b9d52fbc5404f0d8e1922c265944bb1567
AditiNaldurgkar <aditinaldurgkar03@gmail.com>
2024-08-18 23:47:38 +0530

    lib installed and basic file created

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { createRoot } from "react-dom/client";
+const root = createRoot(document.getElementById("root"))
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
+const router=createBrowserRouter([{
+    path:"/",
+    element: 
+}
+{
+    path:"/",
+    element: 
+}
+{
+    path:"/",
+    element: 
+}])
